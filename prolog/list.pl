0b4260ce6395c0282aa8ec9525e342e478afb653
cwsyth <46164156+cwsyth@users.noreply.github.com>
2022-11-29 14:16:13 +0100

    Create list.pl

---
+++ prolog/list.pl
@@ -0,0 +1,14 @@
+len([], 0).
+len([_|R], Res) :-
+   len(R, Res1),
+   Res is Res1 + 1.
+   
+has(K, [K|_]).
+has(K, [_|R]) :- has(K, R).
+
+delete(K, [K|Rest], Rest).
+delete(A, [B|Rest], [B, ARest]) :- delete(A, Rest, ARest).
+
+insert(A, L, R) :- delete(A, R, L).
+
+nelem(1, [K|_])
