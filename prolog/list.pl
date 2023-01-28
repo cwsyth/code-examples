eb3d3bfd0606fc3e923f616218c1cf61fcc358b0
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2023-01-28 22:50:37 +0100

    Update list.pl

--- prolog/list.pl
+++ prolog/list.pl
@@ -2,6 +2,12 @@ mylen([], 0).
 mylen([_|R], Res) :-
 	mylen(R, Res1),
 	Res is Res1+1.
+
+len_rr([], Z, Z).
+len_rr([_|R], Z, E) :-
+    Z1 is Z+1,
+    len(R, Z1, E).
+len_rr(L, E) :- len(L, 0, E).
    
 myhas([H|_], H).
 myhas([_|R], E) :-
@@ -13,6 +19,10 @@ mycopy([H|R1], [H|R2]) :-
 
 mydelete(E, [E|R], R).
 mydelete(E, [H|R1], [H|R2]) :- mydelete(E, R1, R2).
+
+delete_all([], _, []).
+delete_all([E|R1], E, R2) :- delete(R1, E, R2).
+delete_all([K|R1], E, [K|R2]) :- delete(R1, E, R2)
 
 myinsert(E, L, Res) :- Res = [E|L].
 % myinsert(E, L, Res) :- mydelete(E, Res, L).
